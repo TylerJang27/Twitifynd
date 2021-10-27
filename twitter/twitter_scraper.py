09c37ba9b6e8ba835ddf4cb95e04e0339968d98c
Tyler Jang <taj26@duke.edu>
2021-10-27 17:09:27 -0400

    added additional query parameters

--- twitter/twitter_scraper.py
+++ twitter/twitter_scraper.py
@@ -5,16 +5,34 @@
 In order to run this script, first run:
     pip install python-dotenv
     cp template.env .env # edit it to contain the Twitter Bearer token
-    
+   
+API Reference: https://developer.twitter.com/en/docs/api-reference-index
 '''
 
+# %% Imports
 import os
 from sys import exit
 from dotenv import dotenv_values
 import requests
 import time
 
+# %% Constants
 TWITTER_FOLLOWERS_URL = "https://api.twitter.com/2/users/{:}/followers"
+TWITTER_FOLLOWING_URL = "https://api.twitter.com/2/users/{:}/following"
+MAX_RESULTS = "max_results={:}"
+PAGEINATION_TOKEN = "pagination_token={:}"
+USER_FIELDS = "user.fields=id,name,created_at,username,url,verified,description,protected,public_metrics,location"
+
+# https://api.twitter.com/2/users/2244994945/following?expansions=pinned_tweet_id&user.fields=id,name,created_at,username,url,verified,description,protected,public_metrics,location&tweet.fields=created_at&max_results=10
+
+"""
+expansions
+max_results (1-1000)
+pagination_token (meta.next_token)
+user.fields (id, name, username, url, verified, description, protected, public_metrics, location)
+"""
+
+# %% Environment setup
 
 def get_bearer():
     config = dotenv_values(".env")
@@ -33,7 +51,7 @@ def get_bearer():
         exit()
     return twitter_bearer
 
-
+# %% Request logic
 
 def request_to_api(url, bearer):
     headers = {"Authorization": "Bearer {:}".format(bearer)}
@@ -46,6 +64,9 @@ def request_to_api(url, bearer):
 def request_followers(user_id, bearer):
     return request_to_api(TWITTER_FOLLOWERS_URL.format(user_id), bearer)
 
+# %% Parsing logic
+
+# %% Main
 
 if __name__ == "__main__":
     twitter_bearer = get_bearer()
