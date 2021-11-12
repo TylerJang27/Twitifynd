bd8c9baf308c82a0c1691f84952f802ce3ce4bf8
Tyler Jang <taj26@duke.edu>
2021-11-12 01:23:44 -0500

    modified database model

--- db/create.sql
+++ db/create.sql
@@ -1,49 +1,67 @@
-CREATE TABLE testtable (id INT);
-/* DO WE WANT OTHER NON-ARTIST TWITTER USERS? */
+CREATE TABLE testtable (id INT); /* TODO: REMOVE */
 CREATE TABLE artist (
     id int NOT NULL PRIMARY KEY,
-    artist_name varchar(32),
-    twitter_id int NOT NULL,
-    twitter_name varchar(25) NOT NULL, /* MAYBE EXTRACT INTO TWITTER_ARTIST */
-    spotify_id varchar(25) NOT NULL,
-    spotify_name varchar(25) NOT NULL /* MAYBE EXTRACT INTO SPOTIFY_ARTIST */
+    artist_name varchar(50), /* should we scrap this in favor of the spotify name? */
+    twitter_id int NULL, /* I was thinking we may have spotify or twitter users without being linked to start with, so it may be useful to keep this structure */
+    spotify_id char(22) NULL,
+    FOREIGN KEY (twitter_id) REFERENCES twitter_user(twitter_id),
+    FOREIGN KEY (spotify_id) REFERENCES spotify_artist(spotify_id)
 );
-CREATE TABLE twitter_artist(
+CREATE TABLE twitter_user(
     twitter_id int NOT NULL PRIMARY KEY,
-    /* twitter public metrics and any other data we want to collect*/
-    FOREIGN KEY (twitter_id) REFERENCES artist(twitter_id)
-);
-CREATE TABLE spotify_artist(
-    spotify_id int NOT NULL PRIMARY KEY,
-    /* spotify song attribute distribution */
-    FOREIGN KEY (spotify_id) REFERENCES artist(spotify_id)
+    twitter_username varchar(50) NOT NULL,
+    twitter_name varchar(50) NOT NULL,
+    bio varchar(160) NOT NULL,
+    verified boolean NOT NULL,
+    protected boolean NOT NULL,
+    followers_count INT NOT NULL,
+    following_count INT NOT NULL,
+    tweet_count INT NOT NULL,
+    listed_count INT NOT NULL
 );
 CREATE TABLE following(
     follower_id int,
     followed_id int,
     PRIMARY KEY (follower_id, followed_id),
-    FOREIGN KEY (follower_id) REFERENCES artist(twitter_id),
-    FOREIGN KEY (followed_id) REFERENCES artist(twitter_id)
+    FOREIGN KEY (follower_id) REFERENCES twitter_user(twitter_id),
+    FOREIGN KEY (followed_id) REFERENCES twitter_user(twitter_id)
 );
-
-/* TODO: REPLACE WITH CLIENT-SIDE RATHER THAN SERVER SIDE COPY FUNCTION */
-/* export code borrowed from https://stackoverflow.com/questions/17463299/export-postgres-database-into-csv-file */
-CREATE OR REPLACE FUNCTION db_to_csv(path TEXT) RETURNS void AS $$
-declare
-   tables RECORD;
-   statement TEXT;
-begin
-FOR tables IN 
-   SELECT (table_name) AS schema_table
-   FROM information_schema.tables t INNER JOIN information_schema.schemata s 
-   ON s.schema_name = t.table_schema 
-   WHERE t.table_schema NOT IN ('pg_catalog', 'information_schema')
-   AND t.table_type NOT IN ('VIEW')
-   ORDER BY schema_table
-LOOP
-   statement := 'COPY ' || tables.schema_table || ' TO ''' || path || '/' || tables.schema_table || '.csv' ||''' DELIMITER '';'' CSV HEADER';
-   EXECUTE statement;
-END LOOP;
-return;  
-end;
-$$ LANGUAGE plpgsql;
+CREATE TABLE spotify_artist(
+    spotify_id char(22) NOT NULL PRIMARY KEY,
+    spotify_name varchar(50) NOT NULL,
+    genres varchar(64), /* should we make this a many-one separate table or an index or something? */
+    followers INT NOT NULL,
+    popularity INT NOT NULL,
+    mean_danceability FLOAT NOT NULL,
+    sd_danceability FLOAT NOT NULL,
+    mean_energy FLOAT NOT NULL,
+    sd_energy FLOAT NOT NULL,
+    mean_key FLOAT NOT NULL,
+    sd_key FLOAT NOT NULL,
+    mean_loudness FLOAT NOT NULL,
+    sd_loudness FLOAT NOT NULL,
+    mean_mode FLOAT NOT NULL,
+    sd_mode FLOAT NOT NULL,
+    mean_speechiness FLOAT NOT NULL,
+    sd_speechiness FLOAT NOT NULL,
+    mean_acousticness FLOAT NOT NULL,
+    sd_acousticness FLOAT NOT NULL,
+    mean_instrumentalness FLOAT NOT NULL,
+    sd_instrumentalness FLOAT NOT NULL,
+    mean_liveness FLOAT NOT NULL,
+    sd_liveness FLOAT NOT NULL,
+    mean_valence FLOAT NOT NULL,
+    sd_valence FLOAT NOT NULL,
+    mean_tempo FLOAT NOT NULL,
+    sd_tempo FLOAT NOT NULL,
+    mean_duration_ms FLOAT NOT NULL,
+    sd_duration_ms FLOAT NOT NULL,
+    mean_time_signature FLOAT NOT NULL,
+    sd_time_signature FLOAT NOT NULL,
+    mean_chorus_hit FLOAT NOT NULL,
+    sd_chorus_hit FLOAT NOT NULL,
+    mean_sections FLOAT NOT NULL,
+    sd_sections FLOAT NOT NULL,
+    mean_target FLOAT NOT NULL,
+    sd_target FLOAT NOT NULL
+);
