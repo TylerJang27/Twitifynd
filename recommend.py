f9daf9547bd6a6d8f864ec0693496d26c38fc5f2
Caleb Woo <caleb.woo@duke.edu>
2021-11-15 04:46:53 -0500

    precomputation, recommend class, data stored in precomputed

---
+++ recommend.py
@@ -0,0 +1,20 @@
+import json
+import pandas as pd
+
+class Recommend:
+    def __init__(self):
+        with open ('precomputed/artist_info.json') as aif:
+            self.artist_info = json.load(aif)
+
+        with open('precomputed/candidates_scores.json') as csf:
+            self.candidates_scores = json.load(csf)
+    def get_recs(self, artists):
+        df_recs = pd.DataFrame(columns=['id', 'score', 'similar to'])
+        for artist in artists:
+            for candidate in self.candidates_scores.get(artist):
+                df_recs = df_recs.append({
+                    'id': self.artist_info[candidate[0]]['name'],
+                    'score': candidate[1], 'similar to': self.artist_info[artist]['name']
+                }, ignore_index=True)
+        df_recs.sort_values(by='score', ascending=False, inplace=True)
+        return df_recs.head()
